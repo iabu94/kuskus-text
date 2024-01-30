e1b74e20d10ba2b6a39dc269676c30f4f7754992
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2024-01-30 09:53:15 +0530

    Kusu Kus App

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'reverse-text';
+  input = new FormControl('');
+  output = new FormControl('');
+
+  convert() {
+    const text = this.input.value ?? '';
+    const reversedText = text.split('').reverse().join('');
+    const reversedTextSplit = reversedText.split('');
+    for (let i = 0; i < reversedTextSplit.length; i++) {
+      if (i % 2 !== 0) {
+        reversedTextSplit[i] = reversedTextSplit[i].toUpperCase();
+      }
+    }
+    this.output.setValue(reversedTextSplit.join(''));
+  }
+}
